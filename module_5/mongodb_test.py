da987a32ce77083f2cb7c205781610e7944ed46f
Jacob Hayes <github@jakehayes.33mail.com>
2021-01-31 21:05:09 -0500

    Update mongodb_test.py
    Replace mongodb_test.py template with a working version that can connect to a MongoDB database.

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,16 +1,17 @@
 """ 
     Title: mongodb_test.py
-    Author: Professor Krasso
-    Date: 10 July 2020
+    Author: Jacob Hayes
+    Date: 31 Jan 2021
     Description: Test program for connecting to a 
-                 MongoDB Atlas cluster
+                 MongoDB Atlas cluster, using Professor Krasso's
+                 code as a template
 """
 
 """ import statements """
 from pymongo import MongoClient
 
 # MongoDB connection string 
-url = "mongodb+srv://admin:admin@cluster0.rsnru.mongodb.net/pytech?retryWrites=true&w=majority"
+url = "mongodb+srv://admin:admin@cluster0.lazzm.mongodb.net/pytech"
 
 # connect to the MongoDB cluster 
 client = MongoClient(url)
@@ -23,4 +24,4 @@ print("\n -- Pytech COllection List --")
 print(db.list_collection_names())
 
 # show an exit message
-input("\n\n  End of program, press any key to exit... ")
+input("\n\n  End of program, press Enter to exit... ")
